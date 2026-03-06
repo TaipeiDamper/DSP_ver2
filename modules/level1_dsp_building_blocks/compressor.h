3f67088ee721a426d6522eb621dfbe01523c2243
dylan <dylan@asahis.tw>
2026-03-06 17:31:02 +0800

    update 20260306

--- modules/level1_dsp_building_blocks/compressor.h
+++ modules/level1_dsp_building_blocks/compressor.h
@@ -1,3 +1,15 @@
 ﻿// not complete
 
+class Compressor {
+protected:
+  float sampleRate = 44100.0f;
 
+public:
+  Compressor() {};
+  void prepare(float sampleRate) {};
+  void reset() {};
+  float processSample(float input) { return input; };
+  void processBlock(float *buffer, int numSamples) {};
+
+private:
+};
